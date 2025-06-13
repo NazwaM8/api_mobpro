991f9284c0767f5595964e5d823718ac62172a6f
Nazwa Mursyidan Baldan <nizuuichi@gmail.com>
2025-06-13 18:35:01 +0700

    first commit

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -3,12 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Book extends Model
 {
     use HasFactory;
 
-    protected $table = 'books';
+    protected $table = 'book';
     protected $fillable = [
         'Authorization','title', 'author', 'publisher', 'year', 'image'
     ];
